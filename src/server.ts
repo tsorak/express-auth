2da02207c39f2f77e52dd345380f35620c8c98ed
tsorak <karost0249@gmail.com>
2023-02-15 12:53:31 +0100

    Add Express

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+	console.clear();
+	console.log("Server is running on port 3000");
+	console.log("http://localhost:3000");
+});
